356fb29e279284079b9c6d82106a10f117d1e6cd
SPSZerone <12197072+SPSZerone@users.noreply.github.com>
2026-04-19 14:56:08 +0800

    Profile & Theme info

--- src/data/site.ts
+++ src/data/site.ts
@@ -15,7 +15,7 @@ export type SiteLinkCard = {
 
 export const site = {
   seo: {
-    title: 'William Chen - 陈威',
+    title: 'William Chen - 陈威 - SPS',
     ogTitle: '',
     twitterTitle: '',
     description:
@@ -29,16 +29,16 @@ export const site = {
   },
 
   featuredLink: {
-    href: '',
-    title: '',
-    subtitle: '',
+    href: 'https://me.seapianosnow.com/',
+    title: '🧑‍💻 Full-Stack Game Developer',
+    subtitle: '‍🎵 Classical Music 🎹 Piano 🎻 Violin Cello | 🚴‍♀️ Cycling 🏃‍♂️ Running 🎾 Tennis',
   },
 
   profile: {
     avatarSrc: import.meta.env.BASE_URL + 'avatar.png',
-    name: 'William Chen - 陈威',
-    handle: '',
-    bio: '',
+    name: 'William Chen - 陈威 - SPS',
+    handle: '@ https://me.seapianosnow.com/',
+    bio: '🧑‍💻 游戏全栈开发者 | 🎵古典音乐 🎹钢琴 🎻小提琴 大提琴 | 🚴‍♀️骑行 🏃‍♂️跑步 🎾网球',
   },
 
   socialLinks: [
@@ -52,36 +52,32 @@ export const site = {
   linkCards: [
     {
       href: 'https://twilight.me.seapianosnow.com',
-      title: 'Theme - Twilight',
-      subtitle: 'Language, Theme Color, Light/Dark Mode\nGlassmorphism, Modern & Minimalist, Card-based Layout, Fresh & Aesthetic'
-            + '\n语言，主题色，浅色/深色\n毛玻璃拟态，现代简约，卡片式布局，清新唯美',
+      title: 'Twilight (Language, Theme Color, Light/Dark Mode | Glassmorphism, Modern & Minimalist, Card-based Layout, Fresh & Aesthetic)',
+      subtitle: '语言，主题色，浅色/深色 | 毛玻璃拟态，现代简约，卡片式布局，清新唯美',
       icon: '',
       trailingIcon: '',
       style: 'background-color: rgba(250, 204, 21, 0.125); color: rgb(250, 204, 21);',
     },
     {
       href: 'https://darkness.me.seapianosnow.com',
-      title: 'Theme - Darkness',
-      subtitle: 'Modern Animations, Dark Aesthetics, Motion-Rich\nDark Tech / Sci-Fi, Futuristic, Cyber Neon, Deep Space / Starry, Immersive'
-            + '\n现代动画，阴暗美学，动作丰富\n暗黑科技风，未来主义，赛博霓虹，深邃宇宙，沉浸式',
+      title: 'Darkness (Modern Animations, Dark Aesthetics, Motion-Rich\nDark Tech / Sci-Fi, Futuristic, Cyber Neon, Deep Space / Starry, Immersive)',
+      subtitle: '现代动画，阴暗美学，动作丰富 | 暗黑科技风，未来主义，赛博霓虹，深邃宇宙，沉浸式',
       icon: '',
       trailingIcon: '',
       style: 'background-color: rgba(59, 130, 246, 0.125); color: rgb(59, 130, 246);',
     },
     {
       href: 'https://astrogon.me.seapianosnow.com',
-      title: 'Theme - Astrogon',
-      subtitle: 'Language | Geek, Cyberpunk, Sci-Fi, Retro Terminal'
-          + '\n语言 | 极客，赛博朋克，未来科幻，复古终端',
+      title: 'Astrogon (Language | Geek, Cyberpunk, Sci-Fi, Retro Terminal)',
+      subtitle: '语言 | 极客，赛博朋克，未来科幻，复古终端',
       icon: '',
       trailingIcon: '',
       style: 'background-color: rgba(132, 204, 22, 0.125); color: rgb(132, 204, 22);',
     },
     {
       href: 'https://anglefeint.me.seapianosnow.com',
-      title: 'Theme - Anglefeint',
-      subtitle: 'Light/Dark Mode | Glassmorphism, Soothing, Natural, Minimalist, Crisp, Modern, Functional'
-          + '\n浅色/深色 | 毛玻璃特效，治愈，自然，极简，清爽，现代，实用',
+      title: 'Anglefeint (Light/Dark Mode | Glassmorphism, Soothing, Natural, Minimalist, Crisp, Modern, Functional)',
+      subtitle: '浅色/深色 | 毛玻璃特效，治愈，自然，极简，清爽，现代，实用',
       icon: '',
       trailingIcon: '',
       style: 'background-color: rgba(244, 63, 94, 0.125); color: rgb(244, 63, 94);',
