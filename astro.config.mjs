e19bba14390491735e382f7af1b6a401c999aef3
SPSZerone <12197072+SPSZerone@users.noreply.github.com>
2026-04-30 09:11:54 +0800

    page 404

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
+/* global process */
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import cloudflare from '@astrojs/cloudflare';
 
+import icon from 'astro-icon';
+
+const isDev = process.env.NODE_ENV === 'development';
+
 // https://astro.build/config
 export default defineConfig({
+  trailingSlash: isDev ? 'ignore' : 'always',
+  integrations: [icon()],
   vite: {
     plugins: [tailwindcss()],
   },
